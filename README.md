ed131be5c466481dda7b1eb13021851b87670b93
RetaxMaster <retaxmaster@hotmail.com>
2022-10-17 22:17:21 -0500

    Ruleta terminada

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruleta de desafíos con HTML, CSS y JavaScript
+
+Aprende a cómo crear esta ruleta desde 0 con RetaxMaster :D
---
+++ js/script.js
@@ -0,0 +1,41 @@
+const roulette = document.querySelector("#roulette");
+const spinButton = document.querySelector("#spin");
+const resetButton = document.querySelector("#reset");
+
+const maxSpins = 10;
+const minSpins = 1;
+
+const maxDegrees = 360;
+const minDegrees = 1;
+
+const getRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+}
+
+spinButton.addEventListener("click", () => {
+
+    const spins = getRandomNumber(minSpins, maxSpins);
+    const degrees = getRandomNumber(minDegrees, maxDegrees);
+
+    const fullSpins = (spins - 1) * 360;
+    const spin = fullSpins + degrees;
+
+    const animationTime = spins;
+    
+    roulette.style.transform = `rotate(${spin}deg)`;
+    roulette.style.transitionDuration = `${animationTime}s`;
+
+    spinButton.style.display = "none";
+    resetButton.style.display = "inline-block";
+
+});
+
+resetButton.addEventListener("click", () => {
+
+    roulette.style.transform = "rotate(0deg)";
+    roulette.style.transitionDuration = "2s";
+    spinButton.style.display = "inline-block";
+    resetButton.style.display = "none";
+
+
+});
